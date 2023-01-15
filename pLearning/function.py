d45dbf5c076c005292e21e2ecff517ff66c9bbb8
siyamulislam <siyamulislam1@gmail.com>
2023-01-15 13:47:14 +0600

    mul, div

--- pLearning/function.py
+++ pLearning/function.py
@@ -13,13 +13,23 @@ def sub(num1,num2):
   sum=num1+num2
   return sum
 
+def mul_div(num1,num2):
+    mul=num1*num2
+    div=num1/num2
+    return mul,div
+
+
 def main():
     num1 =100
     num2=50.65
     substraction=sub(num1,num2)
+
     
     print("Sum is: {} ".format(add(num1,num2)), end=' & ')
     print("Sub is: {} ".format(substraction))
+    
+    mul,div=mul_div(num1,num2)
+    print("multiply: ",mul, "division: ",div)
 
 main()
 
