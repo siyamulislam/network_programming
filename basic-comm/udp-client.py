3f8d48bf9fc8f9b1b4bd46c3c2b3365160e4c170
siyamulislam <siyamulislam1@gmail.com>
2023-01-19 21:19:25 +0600

    udp client

--- basic-comm/udp-client.py
+++ basic-comm/udp-client.py
@@ -1,7 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 19 12:43:59 2023
+import socket
 
-@author: SiamPC
-"""
+# create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# define the server address and port
+server_address = ("127.0.0.1", 1234)
+
+# send data to the server
+message = "Hello, Server!"
+client_socket.sendto(message.encode(), server_address)
+
+# receive data from the server
+data, server = client_socket.recvfrom(1024)
+
+# print the received data
+print("Received from server: ", data.decode())
+
+# close the socket
+client_socket.close()
